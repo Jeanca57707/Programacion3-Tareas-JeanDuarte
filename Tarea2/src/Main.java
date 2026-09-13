bd9b6dfcb5483536cfa1d9b0a22590ee5cd35ab5
Jeanca57707 <jean_duarte@ucne.edu.do>
2026-09-13 12:59:23 -0400

    Ejercicio Terminado

--- Tarea2/src/Main.java
+++ Tarea2/src/Main.java
@@ -13,13 +13,19 @@ public static void main(String[] args){
   
     Empleado [] empleados = {p1, adm1, m1, p2, adm2, m2, p3, adm3}; 
 
-    int salarioTotal = 0;
+    double salarioTotal = 0;
     int prof = 0;
     int adm = 0;
     int mant = 0;
     Empleado empleadoDelMes = empleados[0];
+
+    System.out.println("\nInformacion de los empleados: \n");
+    for(Empleado e: empleados){
+        System.out.println(e.toString());
+    }
     
     for(int i = 0; i< empleados.length; i++){
+        System.out.println();
 
         salarioTotal += empleados[i].getSalario();
         empleados[i].trabajar();
@@ -39,10 +45,13 @@ public static void main(String[] args){
         if(empleadoDelMes.getSalario() < empleados[i].getSalario()){
             empleadoDelMes = empleados[i];
         }
+        System.out.println();
     }
     System.out.println("\nEl empleado con el sueldo más alto es " + empleadoDelMes.getNombre() + " con un sueldo de: " + empleadoDelMes.getSalario());
+
+    System.out.println("\nLa suma de todos los salarios da un total de: " + salarioTotal);
     
-    System.out.println("\nEstan registrados: \n" + prof + " Profesores \n" +
+    System.out.println("\nEstan registrados: \n\n" + prof + " Profesores \n" +
         adm + " Administrativos \n" + mant + " de Mantenimiento"
     );
 }
